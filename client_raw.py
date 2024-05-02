5dd6abfe83d66487bba406b13c86531679229ec4
Arthur César <arthurcesat@gmail.com>
2024-05-02 11:09:40 -0300

    atualização

--- client_raw.py
+++ client_raw.py
@@ -7,24 +7,25 @@ import config
 def construir_requisicao(tipo):
     req_res_tipo = (0 << 4) | tipo  # 0 para requisição, tipo em 4 bits inferiores
     identificador = random.randint(1, 65535)
-    return req_res_tipo, identificador
+    tamanho_resposta = 0
+    return struct.pack('!BHB', req_res_tipo, identificador, tamanho_resposta)
 
 # Função para construir o cabeçalho UDP
 def construir_cabecalho_udp(porta_origem, porta_destino, tamanho_payload):
     tamanho_udp = 8 + tamanho_payload
-    checksum_udp = 0  # Checksum pode ser zero se não calculado
+    checksum_udp = 0  # Pode ser zero se não calculado
     return struct.pack('!HHHH', porta_origem, porta_destino, tamanho_udp, checksum_udp)
 
 # Função para construir o cabeçalho IP
 def construir_cabecalho_ip(origem, destino, tamanho_payload):
-    versao_e_ihl = (4 << 4) | 5 # 4 para IPv4, 5 para IHL (Internet Header Length) de 5 palavras de 32 bits
-    tipo_servico = 0 # Tipo de serviço (DSCP + ECN), 0 para padrão
-    tamanho_total = 20 + tamanho_payload # Tamanho total do datagrama em bytes
-    identificacao = random.randint(0, 65535) # Identificação do datagrama
-    flags_e_fragment_offset = 0 # Flags e offset de fragmentação
-    ttl = 64 # Tempo de vida do pacote
+    versao_e_ihl = (4 << 4) | 5
+    tipo_servico = 0
+    tamanho_total = 20 + tamanho_payload
+    identificacao = random.randint(0, 65535)
+    flags_e_fragment_offset = 0
+    ttl = 64
     protocolo = 17  # UDP
-    checksum_cabecalho = 0 # Checksum do cabeçalho, pode ser zero se não calculado
+    checksum_cabecalho = 0
     cabecalho_ip_sem_checksum = struct.pack('!BBHHHBBH4s4s', versao_e_ihl, tipo_servico, tamanho_total, identificacao,
                                             flags_e_fragment_offset, ttl, protocolo, checksum_cabecalho,
                                             socket.inet_aton(origem), socket.inet_aton(destino))
@@ -37,13 +38,13 @@ def calcular_checksum(source_string):
     sum = 0
     count = 0
     while count < len(source_string):
-        val = source_string[count + 1] * 256 + source_string[count] # Ordem da rede
-        sum = sum + val 
-        sum = sum & 0xffffffff # Se um carry ocorrer, ele é adicionado ao final
-        count += 2 # Pula 2 bytes
-    sum = (sum >> 16) + (sum & 0xffff) # Desloca 16 bits e soma novamente
-    sum += (sum >> 16) # Soma o carry
-    answer = ~sum & 0xffff # Bitwise NOT e 16 bits
+        val = source_string[count + 1] * 256 + source_string[count]
+        sum = sum + val
+        sum = sum & 0xffffffff
+        count += 2
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+    answer = ~sum & 0xffff
     return answer
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
@@ -61,8 +62,7 @@ try:
             print("Saindo...")
             break
 
-        req_res_tipo, identificador = construir_requisicao(opcao)
-        mensagem = struct.pack('!BHB', req_res_tipo, identificador, 0)  # 0 no tamanho da resposta porque é uma requisição
+        mensagem = construir_requisicao(opcao)
 
         # Construir cabeçalhos
         udp_header = construir_cabecalho_udp(config.CLIENT_PORT, config.SERVER_PORT, len(mensagem))
